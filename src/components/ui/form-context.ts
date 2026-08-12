1be948096b46f3b83c4ba1413abbc30bb71e923c
Christo <217704679+swanepoelchristo@users.noreply.github.com>
2026-08-12 19:51:48 +0200

    fix Fast Refresh form exports

---
+++ src/components/ui/form-context.ts
@@ -0,0 +1,48 @@
+import * as React from "react";
+import {
+  useFormContext,
+  type FieldPath,
+  type FieldValues,
+} from "react-hook-form";
+
+type FormFieldContextValue<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> = {
+  name: TName;
+};
+
+type FormItemContextValue = {
+  id: string;
+};
+
+const FormFieldContext = React.createContext<FormFieldContextValue | null>(null);
+const FormItemContext = React.createContext<FormItemContextValue | null>(null);
+
+const useFormField = () => {
+  const fieldContext = React.useContext(FormFieldContext);
+  const itemContext = React.useContext(FormItemContext);
+  const { getFieldState, formState } = useFormContext();
+
+  if (!fieldContext) {
+    throw new Error("useFormField should be used within <FormField>");
+  }
+
+  if (!itemContext) {
+    throw new Error("useFormField should be used within <FormItem>");
+  }
+
+  const fieldState = getFieldState(fieldContext.name, formState);
+  const { id } = itemContext;
+
+  return {
+    id,
+    name: fieldContext.name,
+    formItemId: `${id}-form-item`,
+    formDescriptionId: `${id}-form-item-description`,
+    formMessageId: `${id}-form-item-message`,
+    ...fieldState,
+  };
+};
+
+export { FormFieldContext, FormItemContext, useFormField };
